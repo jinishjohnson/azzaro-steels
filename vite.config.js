41657ec670c088d736d850772386579227a338d7
jinishjohnson <jinishcena@gmail.com>
2025-02-25 20:56:54 +0530

    Update all changes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host : true,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        compact: true
       }
-    }
+    },
+    chunkSizeWarningLimit: 1600
+  },
+  server: {
+    port: 3000
   }
 })
